aee204022b055d0b5b54db48a5ce12260163472b
Sidhant Gandhi <sidhant.gandhi@gmail.com>
2018-04-19 11:31:31 -0700

    Add tests and complete generation

---
+++ ContactGenerator/ContactGenerator.swift
@@ -0,0 +1,65 @@
+//
+//  ContactGenerator.swift
+//  ContactGenerator
+//
+//  Created by Sidhant Gandhi on 4/19/18.
+//  Copyright © 2018 NewNoetic, Inc. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import PromiseKit
+
+struct BasicContact {
+    var firstName: String
+    var lastName: String
+    var email: String
+    var phone: String
+}
+
+enum ContactGeneratorError: Error {
+    case JSON
+    case Data
+}
+
+struct ContactGenerator {
+    static func generate() -> Promise<[BasicContact]> {
+        return Promise { seal in
+            Alamofire.request("https://randomuser.me/api").responseJSON { response in
+//                print("Request: \(String(describing: response.request))")   // original url request
+//                print("Response: \(String(describing: response.response))") // http url response
+//                print("Result: \(response.result)")                         // response serialization result
+                
+                guard let json = response.result.value else {
+                    seal.reject(ContactGeneratorError.JSON)
+                    return
+                }
+                
+//                print("JSON: \(json)") // serialized json response
+                
+                guard let jsonDictionary = json as? [String: Any] else { seal.reject(ContactGeneratorError.Data); return }
+                
+                guard let jsonArray = jsonDictionary["results"] as? [[String: Any]] else {
+                    seal.reject(ContactGeneratorError.Data)
+                    return
+                }
+                
+                let contacts = jsonArray.compactMap({ (contactData: [String: Any]) -> BasicContact? in
+                    guard let name: [String: String] = contactData["name"] as? [String: String] else { return nil }
+                    guard let firstName = name["first"] else { return nil }
+                    guard let lastName = name["last"] else { return nil }
+                    guard let email = contactData["email"] as? String else { return nil }
+                    guard let phone = contactData["phone"] as? String else { return nil }
+                    
+                    return BasicContact(firstName: firstName, lastName: lastName, email: email, phone: phone)
+                })
+                
+                seal.fulfill(contacts)
+            }
+        }
+    }
+    
+    static func saveToDevice(_ contacts: [BasicContact]) {
+        
+    }
+}
--- ContactGeneratorTests/ContactGeneratorTests.swift
+++ ContactGeneratorTests/ContactGeneratorTests.swift
@@ -8,29 +8,21 @@
 
 import XCTest
 @testable import ContactGenerator
+import PromiseKit
+import Alamofire
 
 class ContactGeneratorTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testGenerator() {
+        let expectation = XCTestExpectation(description: "generate")
+        
+        ContactGenerator.generate().done { (contacts) in
+            print(contacts)
+            expectation.fulfill()
         }
+            .catch { (error) in
+                XCTFail()
+        }
+        
+        wait(for: [expectation], timeout: 10.0)
     }
-    
 }
