ef4a63b3bfbceef96d588a3b75535dbcdb35949f
Sidhant Gandhi <sidhant.gandhi@gmail.com>
2018-04-19 10:22:43 -0700

    Initial commit

---
+++ ContactGenerator/ContactGenerator.h
@@ -0,0 +1,19 @@
+//
+//  ContactGenerator.h
+//  ContactGenerator
+//
+//  Created by Sidhant Gandhi on 4/19/18.
+//  Copyright © 2018 NewNoetic, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ContactGenerator.
+FOUNDATION_EXPORT double ContactGeneratorVersionNumber;
+
+//! Project version string for ContactGenerator.
+FOUNDATION_EXPORT const unsigned char ContactGeneratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ContactGenerator/PublicHeader.h>
+
+
